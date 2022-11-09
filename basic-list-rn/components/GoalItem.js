0de4f18aa6ad948cf58a1704960dc38f4c49b05c
Felix Anducho <felix.anducho74@gmail.com>
2022-11-09 17:29:26 -0600

    Style list item when is pressed for deletion

--- basic-list-rn/components/GoalItem.js
+++ basic-list-rn/components/GoalItem.js
@@ -3,23 +3,31 @@ import { StyleSheet, View, Text, Pressable } from "react-native"
 
 export default function GoalItem({ item, onDeleteItem }) {
     return (
-        <Pressable onPress={onDeleteItem.bind(this, item.id)}>
-            <View style={styles.listItems}>
+        <View style={styles.listItems}>
+            <Pressable
+                android_ripple={{ color: 'white' }}
+                onPress={onDeleteItem.bind(this, item.id)}
+                style={({ pressed }) => pressed && styles.pressedItem}>
                 <Text style={styles.textColor}>{item.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     listItems: {
-        margin: 8,
-        padding: 3,
+        marginTop: 12,
         borderRadius: 6,
         backgroundColor: "#5e0acc",
     },
 
+    pressedItem: {
+        opacity: 0.5,
+    },
+
     textColor: {
         color: "white",
+        padding: 8,
     },
 })
