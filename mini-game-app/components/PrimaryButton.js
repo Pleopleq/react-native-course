f05765500cc5c7340d2667825f5d62705aa678c2
Felix Anducho <felix.anducho74@gmail.com>
2022-11-15 19:24:29 -0600

    Add styling to primary button component

--- mini-game-app/components/PrimaryButton.js
+++ mini-game-app/components/PrimaryButton.js
@@ -1,9 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 
 export default function PrimaryButton({ btnLabel }) {
+    function handleButtonPress() { }
+
     return (
-        <View>
-            <Text>{btnLabel}</Text>
-        </View>
+        <Pressable onPress={handleButtonPress}>
+            <View style={styles.buttonContainer}>
+                <Text style={styles.buttonText}>{btnLabel}</Text>
+            </View>
+        </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        backgroundColor: "#72063c",
+        borderRadius: 28,
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        margin: 4,
+        elevation: 2
+    },
+
+    buttonText: {
+        color: "white",
+        textAlign: "center"
+    }
+})
