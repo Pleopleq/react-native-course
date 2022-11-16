f6ce3cb66808893ec06cbb9a9cf1cc297f350ca4
Felix Anducho <felix.anducho74@gmail.com>
2022-11-15 20:14:14 -0600

    Add background color to main view

--- mini-game-app/App.js
+++ mini-game-app/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen></StartGameScreen>
+    <View style={styles.rootScreen}>
+      <StartGameScreen></StartGameScreen>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  }
+});
