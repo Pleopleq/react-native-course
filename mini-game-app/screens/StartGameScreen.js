227837a90ea59c28a4d5d240bbf61860056964bd
Felix Anducho <felix.anducho74@gmail.com>
2022-11-15 19:11:23 -0600

    Add styling for android and ios in input container

--- mini-game-app/screens/StartGameScreen.js
+++ mini-game-app/screens/StartGameScreen.js
@@ -1,12 +1,30 @@
-import { TextInput, View } from 'react-native'
+import { TextInput, View, StyleSheet } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton';
 
 export default function StartGameScreen() {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             <PrimaryButton btnLabel={'Reset'}></PrimaryButton>
             <PrimaryButton btnLabel={'Confirm'}></PrimaryButton>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        backgroundColor: "#72063c",
+        elevation: 14,
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            heigh: 2,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    }
+})
